1660f640c0bc51e7c3bbbab204cbb5ad1c694933
Allan Pooley <allanpooley@outlook.com>
2017-04-15 13:06:26 +0000

    Implemented api functionality when /whoami route is hit - responding with JSON containing ip, os & language

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+
 var app = express();
 
 app.use(express.static('public'));
@@ -8,6 +9,19 @@ app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
   res.render('index');
+});
+
+app.get('/whoami', function(req, res) {
+  
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+  var os = req.headers["user-agent"].split(/[\(\)]/)[1];
+  
+  var language = req.headers["accept-language"].split(',')[0];
+  
+  res.status(200).send( {"ipaddress" : ip, "language" : language,"software": os} );
+  res.end();
+  
 });
 
 // listen for requests :)
